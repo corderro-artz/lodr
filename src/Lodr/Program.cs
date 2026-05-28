2e6f2dd662e1bd8e78e1c00724bdad1f6ea55354
Corderro Artz <133152220+corderro-artz@users.noreply.github.com>
2026-05-28 02:30:05 -0400

    feat: wire new services and state into DI; update global imports

--- src/Lodr/Program.cs
+++ src/Lodr/Program.cs
@@ -4,16 +4,29 @@ using Lodr.Services;
 using Lodr.State;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Logging;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// Logging
+var logService = new AppLogService();
+builder.Services.AddSingleton<AppLogService>(logService);
+builder.Logging.ClearProviders();
+builder.Logging.AddProvider(logService);
+
+// Services
 builder.Services.AddScoped<OrientationService>();
-builder.Services.AddScoped<FitCalculator>();
-builder.Services.AddScoped<PlacementEngine>();
-builder.Services.AddScoped<AppState>();
+builder.Services.AddScoped<CapacityService>();
+builder.Services.AddScoped<SlotService>();
+
+// State
+builder.Services.AddScoped<WorkspaceState>();
+
+// Interop
 builder.Services.AddScoped<PersistenceInterop>();
+builder.Services.AddScoped<WorkspacePersistenceInterop>();
 builder.Services.AddScoped<VisualizationInterop>();
 
 await builder.Build().RunAsync();
